4b063dca2cfed94b81f74d5a144a5ccf4d662fa0
Jake Whalen <tjw4ra@virginia.edu>
2020-04-28 21:06:01 -0400

    Removed code section breaks

--- explore.py
+++ explore.py
@@ -1,30 +1,30 @@
-# %% Load required packages
+#Load required packages
 # General Packages
 import pandas as pd
 import numpy as np
 import os
 import matplotlib.pyplot as plt
 
-# %% Get file path location
+#Get file path location
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 file_path = os.path.join(__location__, 'online_retail_II.xlsx')
 df_store = os.path.join(__location__, 'df_store')
 
-# %% read in sales data https://archive.ics.uci.edu/ml/datasets/Online+Retail+II
+#read in sales data https://archive.ics.uci.edu/ml/datasets/Online+Retail+II
 sales_09 = pd.read_excel(file_path, sheet_name='Year 2009-2010')
 sales_10 = pd.read_excel(file_path, sheet_name='Year 2010-2011')
 sales = sales_09.append(sales_10).rename(columns={"Customer ID": "CID"})
 
-# %% Check sales data loaded properly (should be 1M+ rows)
+#Check sales data loaded properly (should be 1M+ rows)
 sales.shape
 
-# %% view the first 5 rows
+#view the first 5 rows
 sales.head(5)
 
-# %% Customer ID should be stored as a string
+#Customer ID should be stored as a string
 sales.CID = sales.CID.astype('object')
 
-# %% Summarize Values in Columns
+#Summarize Values in Columns
 #How many orders are there total?
 sales.Invoice.nunique()
 #How many items (StockCode) are there total?
@@ -35,7 +35,7 @@ sales.CID.nunique()
 #Number of different countries?
 sales.Country.nunique()
 
-# %% Missing Values
+#Missing Values
 sales.isna().mean().round(4) * 100 #22% of records mising a CID
 missing_cid = sales.loc[sales.CID.isna()] #Just under 1/4 million
 missing_cid.Country.value_counts() / len(missing_cid) #98% are UK
@@ -46,7 +46,7 @@ sales[['Invoice', 'HasCID']].drop_duplicates().groupby('Invoice').count().sort_v
 sales = sales.dropna()
 sales.isna().mean().round(4) * 100 #no more missing values
 
-# %% Plot Feature Distibutions
+#Plot Feature Distibutions
 
 #Quantity of Each Item Sold
 #Get the total number of each item sold (StockCode)
@@ -55,3 +55,5 @@ qnty_sold = sales.groupby(['StockCode', 'Description']).sum().nlargest(200, 'Qua
 qnty_sold.plot(kind='bar', x='StockCode', y='Quantity')
 qnty_sold.loc[qnty_sold.Quantity >= 50000].plot(kind='bar', x='Description', y='Quantity')
 
+#Monthly Transactions
+transaction_month = pd.to_datetime(sales['InvoiceDate']).dt.to_period('M')
